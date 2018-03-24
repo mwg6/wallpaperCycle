d0079f7b911803194c1122dff1d1577b0a993198
mwg6 <maximgoodman@gmail.com>
2018-03-23 20:07:19 -0400

    Add files via upload

--- psuedoRandom/src/psuedoRandom/middlePanel.java
+++ psuedoRandom/src/psuedoRandom/middlePanel.java
@@ -1,5 +1,6 @@
 package psuedoRandom;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.io.File;
@@ -13,16 +14,20 @@ import javax.swing.JScrollPane;
 public class middlePanel extends JPanel{
 	
 	//give the path to look in. This will be passed to the imgviewer
-	static File a = new File("c:/users/public/pictures/sample pictures");
+	File a = new File("c:/users/public/pictures/sample pictures");
 	//create the imgviewer we will be passing to
-	static imgViewer midPanel = new imgViewer();
+	imgViewer midPanel = new imgViewer();
+	
+	
 	//attempt to format the gridlayout we wish to add
-	static JPanel display = new JPanel(new GridLayout(3,3));
-	JScrollPane wrap = new JScrollPane(display);
+	
+
 	
 	public middlePanel(){
 		//get the files in our ordained directory
 		midPanel.getFiles(a);
+		JPanel display = new JPanel(new GridLayout(midPanel.getFiles()/3,3));
+		JScrollPane wrap = new JScrollPane(display);
 		//get the list of all images in the directory
 		List<Image> imgarray = midPanel.getImages();
 		//within this list add to the JPanel the images
@@ -33,6 +38,10 @@ public class middlePanel extends JPanel{
 			
 		
 	}
+		System.out.println(midPanel.getFiles()/3);
+		wrap.setPreferredSize(new Dimension(1000,1000));
+		
+		
 		add(wrap);
 	}
 	
